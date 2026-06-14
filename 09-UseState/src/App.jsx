2fc7eef68da0adf571128eb489f85d8573c51c76
AmarSinghKatiyar <amarsinghkatiyar1408@gmail.com>
2026-06-15 00:22:34 +0530

    UseState with project

---
+++ 09-UseState/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [num, setnum] = useState(0)
+
+  function Increase(){
+    setnum(num+1)
+  }
+
+  function Decrease(){
+    setnum(num-1)
+  }
+
+  function IncreaseBy5(){
+    setnum(num+5)
+  }
+
+
+  return (
+    <div className='h-screen w-full bg-black py-30 px-25'>
+      <h1 className='bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-cyan-500 mb-15 text-5xl text-center font-bold uppercase'>Play the Game</h1>
+      <h1 className='text-white bg-blue-400  text-center text-9xl min-w-fit mb-20 rounded-2xl drop-shadow-[0_0_15px_#22d3ee]'>{num}</h1>
+      <div className="flex gap-x-5">
+        <button onClick={Increase} className='text-white bg-emerald-600 hover:bg-emerald-700 px-10 py-5 rounded-2xl font-semibold text-2xl hover:drop-shadow-[0_0_15px_lime] cursor-pointer'>Increase</button>
+        <button onClick={Decrease} className='text-white bg-purple-600 hover:bg-purple-700 px-10 py-5 rounded-2xl font-semibold text-2xl hover:drop-shadow-[0_0_15px_pink] cursor-pointer'>Decrease</button>
+        <button onClick={IncreaseBy5} className='text-white bg-blue-600 hover:bg-blue-700 px-10 py-5 rounded-2xl font-semibold text-2xl hover:drop-shadow-[0_0_15px_deepskyblue] cursor-pointer'>Increase by 5</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
