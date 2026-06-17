98e0cc1f8419ff9f903c04afb38f8ea23bd119bb
AmarSinghKatiyar <amarsinghkatiyar1408@gmail.com>
2026-06-17 23:46:31 +0530

    createing react ui game project using tailwind and react

--- 09-UseState/src/App.jsx
+++ 09-UseState/src/App.jsx
@@ -21,10 +21,10 @@ const App = () => {
     <div className='h-screen w-full bg-black py-30 px-25'>
       <h1 className='bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-cyan-500 mb-15 text-5xl text-center font-bold uppercase'>Play the Game</h1>
       <h1 className='text-white bg-blue-400  text-center text-9xl min-w-fit mb-20 rounded-2xl drop-shadow-[0_0_15px_#22d3ee]'>{num}</h1>
-      <div className="flex gap-x-5">
-        <button onClick={Increase} className='text-white bg-emerald-600 hover:bg-emerald-700 px-10 py-5 rounded-2xl font-semibold text-2xl hover:drop-shadow-[0_0_15px_lime] cursor-pointer'>Increase</button>
-        <button onClick={Decrease} className='text-white bg-purple-600 hover:bg-purple-700 px-10 py-5 rounded-2xl font-semibold text-2xl hover:drop-shadow-[0_0_15px_pink] cursor-pointer'>Decrease</button>
-        <button onClick={IncreaseBy5} className='text-white bg-blue-600 hover:bg-blue-700 px-10 py-5 rounded-2xl font-semibold text-2xl hover:drop-shadow-[0_0_15px_deepskyblue] cursor-pointer'>Increase by 5</button>
+      <div className="flex gap-x-15">
+        <button onClick={Increase} className='text-white bg-emerald-600 hover:bg-emerald-700 px-10 py-7 rounded-2xl font-semibold text-3xl hover:drop-shadow-[0_0_15px_lime] cursor-pointer w-100'>Increase</button>
+        <button onClick={Decrease} className='text-white bg-purple-600 hover:bg-purple-700 px-10 py-7 rounded-2xl font-semibold text-3xl hover:drop-shadow-[0_0_15px_pink] cursor-pointer w-100'>Decrease</button>
+        <button onClick={IncreaseBy5} className='text-white bg-blue-600 hover:bg-blue-700 px-10 py-7 rounded-2xl font-semibold text-3xl hover:drop-shadow-[0_0_15px_deepskyblue] cursor-pointer w-100'>Increase by 5</button>
       </div>
     </div>
   )
